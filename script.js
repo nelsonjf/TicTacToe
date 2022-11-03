03dd11ee4443e81b624b642c479fa8c0dc212078
James <jfnelson2000@gmail.com>
2022-11-02 21:17:56 -0700

    added game status

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-console.log("LINK TEST")
-
 let turnX = true
 
-function testFunc (num) {
+function play (num) {
     let html = document.getElementById(num)
+    let status = document.getElementById('status')
 
     if (turnX) {
         html.innerHTML = "X";
+        status.innerHTML = "O's Turn"
         turnX = false
     } else {
         html.innerHTML = "O";
+        status.innerHTML = "X's Turn"
         turnX = true
     }
 
 }
+
