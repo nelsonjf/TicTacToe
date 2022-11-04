cf807a401e4860f32169439edd399d4cc70edf2a
James <jfnelson2000@gmail.com>
2022-11-03 19:13:54 -0700

    prevented players from using spots already taken

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let turnX = true
 
-function play (num) {
-    let html = document.getElementById(num)
+function playGame (num) {
+    let square = document.getElementById(num)
     let status = document.getElementById('status')
 
-    if (turnX) {
-        html.innerHTML = "X";
+    if (square.className === "played") {
+        status.innerHTML = "This spot is taken!"
+
+    } else if (turnX) {
+        square.innerHTML = "X";
         status.innerHTML = "O's Turn"
         turnX = false
+        square.className = "played"
+
     } else {
-        html.innerHTML = "O";
+        square.innerHTML = "O";
         status.innerHTML = "X's Turn"
         turnX = true
+        square.className = "played"
     }
-
 }
 
